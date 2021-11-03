7e7d6096ac35476f2738f744c396d9229d257efd
wlx0048 <1414552264@qq.com>
2021-11-03 16:09:04 +0800

    1

---
+++ Source/bigwork/bigworkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "bigworkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIGWORK_API AbigworkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
